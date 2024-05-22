17a9e05314dad65bbab234509357298809fc339b
teknologi <daffaaryapp@gmail.com>
2024-05-23 00:35:47 +0700

    konfigurasi route permissions

---
+++ src/views/Admin/Permissions/Index.jsx
@@ -0,0 +1,10 @@
+//import layout
+import LayoutAdmin from "../../../layouts/Admin";
+
+export default function PermissionsIndex() {
+  return (
+    <LayoutAdmin>
+      <h1>Halaman Permissions Index</h1>
+    </LayoutAdmin>
+  );
+}
